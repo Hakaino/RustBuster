8c5c4334c219dc5be712e2a8554b7be0cabec59f
Marco Alemão <marco_alemao@hotmail.com>
2023-02-12 01:27:19 +0100

    Make ros2 package

---
+++ src/rustbuster/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'rustbuster'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Marco Alemão',
+    maintainer_email='malema18@student.aau.dk',
+    description='Rust inspection software for Spot robot',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = py_pubsub.publisher_member_function:main',
+            'listener = py_pubsub.subscriber_member_function:main',
+        ],
+    },
+)
